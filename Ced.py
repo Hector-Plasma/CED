c31b0b6f6c2fd27a6b2e6d36476d9bc716dae754
Hector-Plasma <clement.moreau41@gmail.com>
2019-09-08 10:42:12 +0200

    Update Ced.py

--- Ced.py
+++ Ced.py
@@ -5,7 +5,7 @@ from Operator import *
 from Ontology import *
 from Contextual_function import *
 
-def cedDP(S1, S2, f, g) :
+def one_sided_CED(S1, S2, f, sim) :
     D = np.zeros((len(S1) + 1, len(S2) + 1))
     for i in range(len(S1) + 1):
         for j in range(len(S2) + 1):
@@ -13,51 +13,76 @@ def cedDP(S1, S2, f, g) :
                 D[i, j] = 0
             else:
                 if (i == 0):
-                    e = Operator("del", S1, 0, S2, j-1)
-                    D[i, j] = D[i, j - 1] + e.cost_edit(f, g.sim_Wu_Palmer)
+                    e = Operator(Edit_operator.ADD, S1, S2[j - 1], 0)
+                    D[i, j] = D[i, j - 1] + e.cost_edit(f, sim)
                 if (j == 0):
-                    e = Operator("add", S2, 0, S1, i - 1)
-                    D[i, j] = D[i - 1, j] + e.cost_edit(f, g.sim_Wu_Palmer)
+                    e = Operator(Edit_operator.DEL, S1, S1[i - 1], i - 1)
+                    D[i, j] = D[i - 1, j] + e.cost_edit(f, sim)
                 if (i != 0 and j != 0):
 
-                    e_mod = Operator("mod", S2, j - 1, S1, i - 1)
-                    e_del = Operator("del", S2, j - 2, S1, i - 1)
-                    e_add = Operator("add", S1, i - 2, S2, j - 1)
-                    cost_mod = e_mod.cost_edit(f, g.sim_Wu_Palmer)
-                    cost_del = e_del.cost_edit(f, g.sim_Wu_Palmer)
-                    cost_add = e_add.cost_edit(f, g.sim_Wu_Palmer)
+                    cost_mod = Operator(Edit_operator.MOD, S1, S2[j - 1], i - 1).cost_edit(f, sim)
+                    cost_del = Operator(Edit_operator.DEL, S1, S1[i - 1], i - 1).cost_edit(f, sim)
+                    cost_add = Operator(Edit_operator.ADD, S1, S2[j - 1], i - 2).cost_edit(f, sim)
 
                     D[i, j] = min(D[i - 1, j - 1] + cost_mod,
                                   D[i - 1, j] + cost_del,
                                   D[i, j - 1] + cost_add)
+    #print D
     return D[len(S1), len(S2)]
 
+
+#----- Main -----#
+
 onto = Ontology('ontologie.txt')
+#onto.print_ontology()
 
-S1 = [onto.graph.vs()[17], onto.graph.vs()[14]]
-S2 = [onto.graph.vs()[13], onto.graph.vs()[14]]
-S3 = [onto.graph.vs()[15], onto.graph.vs()[7]]
-S4 = [onto.graph.vs()[15], onto.graph.vs()[8]]
-S5 = [onto.graph.vs()[15], onto.graph.vs()[9]]
-S6 = [onto.graph.vs()[7], onto.graph.vs()[16], onto.graph.vs()[10]]
-S7 = [onto.graph.vs()[7], onto.graph.vs()[13], onto.graph.vs()[10]]
-S8 = [onto.graph.vs()[7], onto.graph.vs()[16], onto.graph.vs()[7], onto.graph.vs()[17], onto.graph.vs()[10]]
-S9 = [onto.graph.vs()[7], onto.graph.vs()[13], onto.graph.vs()[7], onto.graph.vs()[17], onto.graph.vs()[10]]
-S10 = [onto.graph.vs()[8]]
-S11 = [onto.graph.vs()[8], onto.graph.vs()[11], onto.graph.vs()[8]]
-S12 = [onto.graph.vs()[7], onto.graph.vs()[17], onto.graph.vs()[7], onto.graph.vs()[13], onto.graph.vs()[10]]
-S13 = [onto.graph.vs()[10], onto.graph.vs()[16], onto.graph.vs()[7], onto.graph.vs()[17], onto.graph.vs()[7]]
-S14 = [onto.graph.vs()[8], onto.graph.vs()[12], onto.graph.vs()[8]]
-S15 = [onto.graph.vs()[7], onto.graph.vs()[9], onto.graph.vs()[14], onto.graph.vs()[8], onto.graph.vs()[12]]
+"""
+0,{Activity}
+1,{Move Activity}
+2,{Shopping}
+3,{Cultural Activity}
+4,{Sport Activity}
+5,{Human powered transports}
+6,{Motor Vehicles}
+7,{Walk}
+8,{Cycle}
+9,{Drive car}
+10,{Take bus}
+11,{Bakery}
+12,{Other kind of shoppinf}
+13,{Cinema}
+14,{Danse}
+15,{Librairy}
+16,{Swim}
+17,{Play Football}
+"""
 
-Sequences = [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15]
+S1 = [onto.graph.vs()[8]]
+S2 = [onto.graph.vs()[8], onto.graph.vs()[11], onto.graph.vs()[8]]
+S3 = [onto.graph.vs()[7], onto.graph.vs()[12], onto.graph.vs()[7]]
+S4 = [onto.graph.vs()[17], onto.graph.vs()[7], onto.graph.vs()[10], onto.graph.vs()[13]]
+S5 = [onto.graph.vs()[10], onto.graph.vs()[13], onto.graph.vs()[7], onto.graph.vs()[17]]
+S6 = [onto.graph.vs()[7], onto.graph.vs()[16], onto.graph.vs()[10], onto.graph.vs()[13]]
+S7 = [onto.graph.vs()[7], onto.graph.vs()[15], onto.graph.vs()[7], onto.graph.vs()[13]]
+S8 = [onto.graph.vs()[14], onto.graph.vs()[7], onto.graph.vs()[15]]
+S9 = [onto.graph.vs()[8], onto.graph.vs()[16], onto.graph.vs()[8], onto.graph.vs()[15]]
+S10 = [onto.graph.vs()[15], onto.graph.vs()[9], onto.graph.vs()[10], onto.graph.vs()[12]]
 
-CED_matrix = np.zeros((len(Sequences), len(Sequences)))
+Sequences = [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10]
+
+dist_matrix = np.zeros((len(Sequences), len(Sequences)))
 f = Contextual_function().gaussian
+# Do not forget to custom parameters of CED in : 
+# alpha : Operator.py
+# f     : Contextual_function.py
 
 for i in range(len(Sequences)) :
     for j in range(len(Sequences)):
-        S1 = Sequences[i]
-        S2 = Sequences[j]
-        CED_matrix[i,j] = max(cedDP(S1, S2, f, onto), cedDP(S1, S2, f, onto))
-print(CED_matrix)
+        S_1 = Sequences[i]
+        S_2 = Sequences[j]
+        dist_matrix[i,j] = max(one_sided_CED(S_1, S_2, f, onto.sim_wu_Palmer), one_sided_CED(S_2, S_1, f, onto.sim_wu_Palmer))
+
+print(dist_matrix)
+file_name = "your_file_name_here" # 
+
+np.savetxt(file_name, dist_matrix, delimiter=",")
